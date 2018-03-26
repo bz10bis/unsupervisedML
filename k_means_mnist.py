9f7f89a26e2c441dba0a6852a82065f7abb8ec51
Gerome CABEZAS <gerome.cabezas@gmail.com>
2018-03-26 15:09:01 +0200

    K-means ta vie

---
+++ k_means_mnist.py
@@ -0,0 +1,9 @@
+import numpy as np
+from keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+
+print(X_test)
